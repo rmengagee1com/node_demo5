c31e645286183e125b684321097dd89862652516
Wardell <rm@engagee1.com>
2021-03-15 19:15:50 -0700

    updated mobile menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,12 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+/* When you create a new class from an imported object store it in it's own variable for later use.*/
+
+let mobileMenu = new MobileMenu();
 
 if(module.hot){
 	module.hot.accept()
 }
+
+
